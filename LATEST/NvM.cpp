df9d5e20c67807eedd61c4efd6eb7c3aa611425c
root <nagaraja.hm.0001@gmail.com>
2021-12-22 10:57:32 +0100

    Update: submodule NvM

--- LATEST/NvM.cpp
+++ LATEST/NvM.cpp
@@ -5,3 +5,72 @@ class_NvM NvM;
 void class_NvM::InitFunction(void){
 }
 
+void class_NvM::MainFunction(void){
+}
+
+void class_NvM::SetDataIndex(void){
+}
+
+void class_NvM::GetDataIndex(void){
+}
+
+void class_NvM::SetBlockProtection(void){
+}
+
+void class_NvM::GetErrorStatus(void){
+}
+
+void class_NvM::GetVersionInfo(void){
+}
+
+void class_NvM::SetRamBlockStatus(void){
+}
+
+void class_NvM::SetBlockLockStatus(void){
+}
+
+void class_NvM::CancelJobs(void){
+}
+
+void class_NvM::ReadBlock(void){
+}
+
+void class_NvM::WriteBlock(void){
+}
+
+void class_NvM::RestoreBlockDefaults(void){
+}
+
+void class_NvM::EraseNvBlock(void){
+}
+
+void class_NvM::CancelWriteAll(void){
+}
+
+void class_NvM::InvalidateNvBlock(void){
+}
+
+void class_NvM::ReadPRAMBlock(void){
+}
+
+void class_NvM::WritePRAMBlock(void){
+}
+
+void class_NvM::RestorePRAMBlockDefaults(void){
+}
+
+void class_NvM::ReadAll(void){
+}
+
+void class_NvM::WriteAll(void){
+}
+
+void class_NvM::ValidateAll(void){
+}
+
+void class_NvM::JobEndNotification(void){
+}
+
+void class_NvM::JobErrorNotification(void){
+}
+
--- LATEST/NvM.h
+++ LATEST/NvM.h
@@ -2,7 +2,30 @@
 
 class class_NvM{
    public:
-      void InitFunction(void);
+      void InitFunction             (void);
+      void MainFunction             (void);
+      void SetDataIndex             (void);
+      void GetDataIndex             (void);
+      void SetBlockProtection       (void);
+      void GetErrorStatus           (void);
+      void GetVersionInfo           (void);
+      void SetRamBlockStatus        (void);
+      void SetBlockLockStatus       (void);
+      void CancelJobs               (void);
+      void ReadBlock                (void);
+      void WriteBlock               (void);
+      void RestoreBlockDefaults     (void);
+      void EraseNvBlock             (void);
+      void CancelWriteAll           (void);
+      void InvalidateNvBlock        (void);
+      void ReadPRAMBlock            (void);
+      void WritePRAMBlock           (void);
+      void RestorePRAMBlockDefaults (void);
+      void ReadAll                  (void);
+      void WriteAll                 (void);
+      void ValidateAll              (void);
+      void JobEndNotification       (void);
+      void JobErrorNotification     (void);
 };
 
 extern class_NvM NvM;
