fc6e3ed1de08276c8d5359834f9d944aa7ec5520
root <nagaraja.hm.0001@gmail.com>
2021-12-23 23:23:21 +0100

    Update: submodule NvM

--- LATEST/NvM.cpp
+++ LATEST/NvM.cpp
@@ -1,11 +1,14 @@
 #include "NvM.h"
 
+#include "MemIf.h"
+
 class_NvM NvM;
 
 void class_NvM::InitFunction(void){
 }
 
 void class_NvM::MainFunction(void){
+   //EcuM.CB_NotifyNvMJobEnd();
 }
 
 void class_NvM::SetDataIndex(void){
@@ -30,12 +33,15 @@ void class_NvM::SetBlockLockStatus(void){
 }
 
 void class_NvM::CancelJobs(void){
+   MemIf.Cancel();
 }
 
 void class_NvM::ReadBlock(void){
+   MemIf.Read();
 }
 
 void class_NvM::WriteBlock(void){
+   MemIf.Write();
 }
 
 void class_NvM::RestoreBlockDefaults(void){
@@ -45,24 +51,29 @@ void class_NvM::EraseNvBlock(void){
 }
 
 void class_NvM::CancelWriteAll(void){
+   MemIf.Cancel();
 }
 
 void class_NvM::InvalidateNvBlock(void){
 }
 
 void class_NvM::ReadPRAMBlock(void){
+   MemIf.Read();
 }
 
 void class_NvM::WritePRAMBlock(void){
+   MemIf.Write();
 }
 
 void class_NvM::RestorePRAMBlockDefaults(void){
 }
 
 void class_NvM::ReadAll(void){
+   MemIf.Read();
 }
 
 void class_NvM::WriteAll(void){
+   MemIf.Write();
 }
 
 void class_NvM::ValidateAll(void){
