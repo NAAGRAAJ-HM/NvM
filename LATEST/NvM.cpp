6174d1d89e921d1d9e35fe41be9e6828ca745597
root <nagaraja.hm.0001@gmail.com>
2022-03-31 12:05:00 +0200

    Update: AUTOSAR basic software service module NvM

--- LATEST/NvM.cpp
+++ LATEST/NvM.cpp
@@ -37,10 +37,9 @@ class module_NvM:
    public:
       module_NvM(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
-      FUNC(void, _CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      FUNC(void, NVM_CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, NVM_CONFIG_DATA, NVM_APPL_CONST) lptrCfgModule
       );
-      FUNC(void, NVM_CODE) InitFunction   (void);
       FUNC(void, NVM_CODE) DeInitFunction (void);
       FUNC(void, NVM_CODE) MainFunction   (void);
 };
@@ -77,23 +76,39 @@ VAR(module_NvM, NVM_VAR) NvM(
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
 FUNC(void, NVM_CODE) module_NvM::InitFunction(
-   CONSTP2CONST(CfgNvM_Type, CFGNVM_CONFIG_DATA, CFGNVM_APPL_CONST) lptrCfgNvM
+   CONSTP2CONST(CfgModule_TypeAbstract, NVM_CONFIG_DATA, NVM_APPL_CONST) lptrCfgModule
 ){
-   if(NULL_PTR == lptrCfgNvM){
+   if(E_OK == IsInitDone){
 #if(STD_ON == NvM_DevErrorDetect)
       Det_ReportError(
       );
 #endif
    }
    else{
-// check lptrCfgNvM for memory faults
+      if(NULL_PTR == lptrCfgModule){
+#if(STD_ON == NvM_DevErrorDetect)
+         Det_ReportError(
+         );
+#endif
+      }
+      else{
+// check lptrCfgModule for memory faults
 // use PBcfg_NvM as back-up configuration
+      }
+      IsInitDone = E_OK;
    }
-   NvM.IsInitDone = E_OK;
 }
 
 FUNC(void, NVM_CODE) module_NvM::DeInitFunction(void){
-   NvM.IsInitDone = E_NOT_OK;
+   if(E_OK != IsInitDone){
+#if(STD_ON == NvM_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+      IsInitDone = E_NOT_OK;
+   }
 }
 
 FUNC(void, NVM_CODE) module_NvM::MainFunction(void){
