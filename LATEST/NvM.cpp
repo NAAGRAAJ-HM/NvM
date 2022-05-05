9d41a5b0a3a27ee3250c9e70f81db22b535d6301
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:09:41 +0000

    Update: AUTOSAR basic software service module NvM

--- LATEST/NvM.cpp
+++ LATEST/NvM.cpp
@@ -85,12 +85,10 @@ FUNC(void, NVM_CODE) module_NvM::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgNvM;
          }
       }
