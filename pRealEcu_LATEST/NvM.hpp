97d17bbaab84d390e3b0093fa596cd03b71f5e0a
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-11-02 15:43:37 +0100

    AUTOSAR Migration Activities

--- pRealEcu_LATEST/NvM.hpp
+++ pRealEcu_LATEST/NvM.hpp
@@ -1,17 +1,11 @@
-
-
 #if(!defined NVM_H_)
 #define NVM_H_
 
-#define NVM_VENDOR_ID              (30u)
+#define NVM_VENDOR_ID              (1u)
 #define NVM_MODULE_ID              (20u)
-
 #define NVM_AR_RELEASE_MAJOR_VERSION       (4u)
 #define NVM_AR_RELEASE_MINOR_VERSION       (2u)
 #define NVM_AR_RELEASE_REVISION_VERSION    (2u)
-//#define NVM_AR_RELEASE_MINOR_VERSION       (0u)
-//#define NVM_AR_RELEASE_REVISION_VERSION    (3u)
-
 #define NVM_SW_MAJOR_VERSION       (5u)
 #define NVM_SW_MINOR_VERSION       (7u)
 #define NVM_SW_PATCH_VERSION       (0u)
@@ -23,15 +17,10 @@
 #define NVM_E_PARAM_BLOCK_TYPE          (0x0Bu)
 #define NVM_E_PARAM_BLOCK_DATA_IDX      (0x0Cu)
 #define NVM_E_PARAM_ADDRESS             (0x0Du)
-
 #define NVM_E_PARAM_DATA                (0x0Eu)
-
 #define NVM_E_NOT_INITIALIZED           (0x14u)
 #define NVM_E_BLOCK_PENDING             (0x15u)
 #define NVM_E_BLOCK_CONFIG              (0x18u)
-
-#define NVM_START_SEC_CODE
-#include "MemMap.hpp"
 
 extern FUNC(void, NVM_PUBLIC_CODE) NvM_Init(void);
 
@@ -47,14 +36,11 @@ extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_GetDataIndex
    ,     P2VAR(uint8, AUTOMATIC, NVM_APPL_DATA) DataIndexPtr
     );
 
-#if(NVM_API_CONFIG_CLASS == NVM_API_CONFIG_CLASS_3)
-
 extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_SetBlockProtection
     (
         NvM_BlockIdType BlockId
    ,     boolean ProtectionEnabled
     );
-#endif
 
 extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_GetErrorStatus
     (
@@ -62,24 +48,16 @@ extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_GetErrorStatus
    ,     P2VAR(NvM_RequestResultType, AUTOMATIC, NVM_APPL_DATA) RequestResultPtr
     );
 
-#if(NVM_VERSION_INFO_API == STD_ON)
-
 extern FUNC(void, NVM_PUBLIC_CODE) NvM_GetVersionInfo
     (
         P2VAR(Std_VersionInfoType, AUTOMATIC, NVM_APPL_DATA) Versioninfo
     );
-#endif
-
-#if(NVM_SET_RAM_BLOCK_STATUS_API == STD_ON)
 
 extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_SetRamBlockStatus
     (
         NvM_BlockIdType BlockId
    ,     boolean BlockChanged
     );
-#endif
-
-#if(NVM_API_CONFIG_CLASS > NVM_API_CONFIG_CLASS_1)
 
 extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_ReadBlock
     (
@@ -98,9 +76,6 @@ extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_RestoreBlockDefaults
         NvM_BlockIdType BlockId
    ,     P2VAR(void, AUTOMATIC, NVM_APPL_DATA) NvM_DstPtr
     );
-#endif
-
-#if(NVM_API_CONFIG_CLASS == NVM_API_CONFIG_CLASS_3)
 
 extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_EraseNvBlock
     (
@@ -111,32 +86,16 @@ extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_InvalidateNvBlock
     (
         NvM_BlockIdType BlockId
     );
-#endif
-
-#if(NVM_API_CONFIG_CLASS > NVM_API_CONFIG_CLASS_1)
 
 extern FUNC(Std_ReturnType, NVM_PUBLIC_CODE) NvM_CancelJobs
     (
         NvM_BlockIdType BlockId
     );
-#endif
 
 extern FUNC(void, NVM_PUBLIC_CODE) NvM_ReadAll(void);
-
 extern FUNC(void, NVM_PUBLIC_CODE) NvM_WriteAll(void);
-
 extern FUNC(void, NVM_PUBLIC_CODE) NvM_CancelWriteAll(void);
-
-#if(NVM_KILL_WRITEALL_API == STD_ON)
-
 extern FUNC(void, NVM_PUBLIC_CODE) NvM_KillWriteAll(void);
-#endif
-
-#if(NVM_REPAIR_REDUNDANT_BLOCKS_API == STD_ON)
-
-FUNC(void, NVM_PUBLIC_CODE) NvM_RepairRedundantBlocks(void);
-#endif
-
 extern FUNC(void, NVM_PUBLIC_CODE) NvM_MainFunction(void);
 
 extern FUNC(void, NVM_PUBLIC_CODE) NvM_SetBlockLockStatus
@@ -144,9 +103,6 @@ extern FUNC(void, NVM_PUBLIC_CODE) NvM_SetBlockLockStatus
         NvM_BlockIdType BlockId
    ,     boolean BlockLocked
     );
-
-#define NVM_STOP_SEC_CODE
-#include "MemMap.hpp"
 
 #endif
 
